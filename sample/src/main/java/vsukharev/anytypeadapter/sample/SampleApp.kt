b9f4cd41023f4e22d19d9dc23536efa7868977bf
Vadim Sukharev <vadimsukharev1@yandex.ru>
2020-05-27 20:49:51 +0300

    cicerone instance is injected into the application class

--- sample/src/main/java/vsukharev/anytypeadapter/sample/SampleApp.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/SampleApp.kt
@@ -1,8 +1,13 @@
 package vsukharev.anytypeadapter.sample
 
 import android.app.Application
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
+import javax.inject.Inject
 
 class SampleApp : Application() {
+    @Inject
+    lateinit var cicerone: Cicerone<Router>
 
     override fun onCreate() {
         super.onCreate()
