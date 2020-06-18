80f7d7803488870b01a59fbd84a22c0f13b68141
Vadim Sukharev <vadimsukharev1@yandex.ru>
2020-06-18 22:46:33 +0300

    made function suspendable

--- sample/src/main/java/vsukharev/anytypeadapter/sample/albums/domain/interactor/AlbumsInteractor.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/albums/domain/interactor/AlbumsInteractor.kt
@@ -1,10 +1,19 @@
 package vsukharev.anytypeadapter.sample.albums.domain.interactor
 
+import vsukharev.anytypeadapter.sample.common.errorhandling.Result
 import vsukharev.anytypeadapter.sample.albums.data.AlbumsRepository
+import vsukharev.anytypeadapter.sample.albums.di.AlbumsScope
 import vsukharev.anytypeadapter.sample.albums.domain.model.Album
 import javax.inject.Inject
 
 class AlbumsInteractor @Inject constructor(private val repository: AlbumsRepository) {
 
-    suspend fun getAlbumsBasedOnPreferences(): List<Album> = repository.getAlbumsBasedOnPreferences()
+    suspend fun getAlbumsBasedOnPreferences(): Result<List<Album>> {
+        return try {
+            Result.Success(repository.getAlbumsBasedOnPreferences())
+        }
+        catch (e: Throwable) {
+            Result.Failure(e)
+        }
+    }
 }
