063c98b6898d4227a0e5dcd874deb1e7ca16de60
Vadim Sukharev <vadimsukharev1@yandex.ru>
2020-07-05 18:21:35 +0300

    fixed comparison methods implementation

--- sample/src/main/java/vsukharev/anytypeadapter/sample/albums/presentation/view/adapter/AlbumsSectionAdapterItem.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/albums/presentation/view/adapter/AlbumsSectionAdapterItem.kt
@@ -6,21 +6,7 @@ import vsukharev.anytypeadapter.item.AdapterItem
  * The [AdapterItem] wrapping the item representing "based on preferences" section
  */
 data class AlbumsSectionAdapterItem(val adapterAlbums: List<AlbumAdapterItem>) : AdapterItem {
-    override fun areItemsTheSame(other: AdapterItem): Boolean {
-        adapterAlbums.forEach {
-            if (!it.areItemsTheSame(other)) {
-                return false
-            }
-        }
-        return true
-    }
+    override fun areItemsTheSame(other: AdapterItem): Boolean = true
 
-    override fun areContentsTheSame(other: AdapterItem): Boolean {
-        adapterAlbums.forEach {
-            if (!it.areContentsTheSame(other)) {
-                return false
-            }
-        }
-        return true
-    }
+    override fun areContentsTheSame(other: AdapterItem): Boolean = false
 }
