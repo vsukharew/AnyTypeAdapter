472d19c39eb84ae1e889c1a52ad2ffb180552dd1
Vadim Sukharev <vadimsukharev1@yandex.ru>
2020-07-05 18:37:36 +0300

    added the additional type check due to the glide use

--- sample/src/main/java/vsukharev/anytypeadapter/sample/common/extension/FragmentExtensions.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/common/extension/FragmentExtensions.kt
@@ -3,9 +3,10 @@ package vsukharev.anytypeadapter.sample.common.extension
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.bumptech.glide.manager.SupportRequestManagerFragment
 
 fun FragmentManager.getFirstVisibleFragment(): Fragment? {
-    return fragments.firstOrNull { !it.isHidden }
+    return fragments.firstOrNull { !it.isHidden && it !is SupportRequestManagerFragment }
 }
 
 fun FragmentTransaction.commitShowAndHide(
