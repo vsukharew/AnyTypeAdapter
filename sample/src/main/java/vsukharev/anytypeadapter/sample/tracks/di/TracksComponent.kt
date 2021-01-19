033b03977b368b56fc4185797b725229544b433f
Vadim Sukharev <vadimsukharev1@yandex.ru>
2021-01-19 22:10:53 +0300

    fix di error

--- sample/src/main/java/vsukharev/anytypeadapter/sample/tracks/di/TracksComponent.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/tracks/di/TracksComponent.kt
@@ -1,11 +1,11 @@
 package vsukharev.anytypeadapter.sample.tracks.di
 
 import dagger.Component
-import vsukharev.anytypeadapter.sample.common.di.common.PerScreen
 import vsukharev.anytypeadapter.sample.tracks.presentation.view.TracksFragment
+import javax.inject.Singleton
 
 @Component
-@PerScreen
+@Singleton
 interface TracksComponent {
     fun inject(fragment: TracksFragment)
 }
