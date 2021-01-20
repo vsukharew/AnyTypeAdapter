5d1e760279bfe7b1cc3d7109476628d5f892f9ad
Vadim Sukharev <vadimsukharev1@yandex.ru>
2021-01-20 22:07:25 +0300

    added error and empty states imitation

--- sample/src/main/java/vsukharev/anytypeadapter/sample/tracks/domain/interactor/TracksInteractor.kt
+++ sample/src/main/java/vsukharev/anytypeadapter/sample/tracks/domain/interactor/TracksInteractor.kt
@@ -6,20 +6,16 @@ import vsukharev.anytypeadapter.sample.tracks.data.TracksRepository
 import vsukharev.anytypeadapter.sample.tracks.domain.model.Track
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.random.Random
 
 @Singleton
 class TracksInteractor @Inject constructor(private val repo: TracksRepository) {
 
     suspend fun getTracks(offset: Int, count: Int): Result<List<Track>> {
         delay(5000L)
-        return if (offset == 0) {
+        return try {
             Result.Success(repo.getTracks(offset, count))
-        } else {
-            when (Random(System.currentTimeMillis()).nextInt(3) % 3) {
-                0 -> Result.Failure(Exception())
-                else -> Result.Success(repo.getTracks(offset, count))
-            }
+        } catch (e: Exception) {
+            Result.Failure(e)
         }
     }
 }
