3349ad9c6cd9394a19ac4135ff098d951c271593
vsukharew <vadimsukharev1@yandex.ru>
2021-10-27 22:03:56 +0300

    added assertion

--- library/src/test/java/vsukharev/anytypeadapter/AnyTypeCollectionBuilderTest.kt
+++ library/src/test/java/vsukharev/anytypeadapter/AnyTypeCollectionBuilderTest.kt
@@ -48,10 +48,10 @@ class AnyTypeCollectionBuilderTest {
         var itemsCount = 0
         var itemsMetadataCount = 0
         AnyTypeCollection.Builder()
-            .addIf(headerDelegate) { true }.also { itemsCount++ ; itemsMetadataCount++ }
-            .addIf(headerDelegate) { false }.also { itemsCount++}
+            .addIf(headerDelegate) { true }.also { itemsCount++; itemsMetadataCount++ }
+            .addIf(headerDelegate) { false }.also { itemsCount++ }
             .build()
-            .apply { itemsMetaData.size == 1 && size == 1 }
+            .apply { assert(itemsMetaData.size == itemsMetadataCount && size != itemsCount && size == 1) }
     }
 
     @Test
