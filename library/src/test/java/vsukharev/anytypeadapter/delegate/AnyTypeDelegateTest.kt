6dab946ee66f3554437bfb5ec5beeecae2b4c4ae
vsukharew <vadimsukharev1@yandex.ru>
2021-11-02 23:01:55 +0300

    fixed AnyTypeDelegateTest

--- library/src/test/java/vsukharev/anytypeadapter/delegate/AnyTypeDelegateTest.kt
+++ library/src/test/java/vsukharev/anytypeadapter/delegate/AnyTypeDelegateTest.kt
@@ -1,16 +1,20 @@
 package vsukharev.anytypeadapter.delegate
 
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.verify
 import vsukharev.anytypeadapter.common.MockInitializer
+import vsukharev.anytypeadapter.domain.Track
 import vsukharev.anytypeadapter.item.AdapterItem
 
 class AnyTypeDelegateTest : MockInitializer() {
 
     @Test
-    fun bind_callWithAnyData_verifyHolderBindGetCalled() {
-        trackDelegate.bind(AdapterItem(any(), any()), trackHolder)
-        verify(trackHolder).bind(any())
+    fun bind_callWithTrack_verifyHolderBindGetCalledWithTheSameTrack() {
+        val track = Track()
+        val captor = argumentCaptor<Track>()
+        trackDelegate.bind(AdapterItem(track.id, track), trackHolder)
+        verify(trackHolder).bind(captor.capture())
+        assert(track == captor.firstValue)
     }
 }
