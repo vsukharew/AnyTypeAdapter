cce3a2c27f43838848572e76067f564ef0b6598d
vsukharew <vadimsukharev1@yandex.ru>
2021-12-11 22:07:31 +0300

    implemented functions that run before and after all tests

--- library/src/test/java/vsukharev/anytypeadapter/adapter/AnyTypeAdapterTest.kt
+++ library/src/test/java/vsukharev/anytypeadapter/adapter/AnyTypeAdapterTest.kt
@@ -1,32 +1,20 @@
 package vsukharev.anytypeadapter.adapter
 
 import androidx.viewbinding.ViewBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.*
-import org.mockito.Spy
-import org.mockito.kotlin.any
 import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.mock
-import vsukharev.anytypeadapter.common.CoroutineDispatcherRule
+import vsukharev.anytypeadapter.common.MainDispatcherExtension
 import vsukharev.anytypeadapter.common.MockInitializer
-import vsukharev.anytypeadapter.delegate.AnyTypeDelegate
 import vsukharev.anytypeadapter.domain.Track
 import vsukharev.anytypeadapter.holder.AnyTypeViewHolder
-import java.util.*
 
+@ExtendWith(MainDispatcherExtension::class)
 class AnyTypeAdapterTest : MockInitializer() {
+
+    private val dispatcher = TestCoroutineDispatcher()
 
     @Test
     fun `onBindViewHolder - call with non-empty payload - verify holder applyPayload get called`() {
@@ -36,5 +24,32 @@ class AnyTypeAdapterTest : MockInitializer() {
         adapter.onBindViewHolder(trackHolder as AnyTypeViewHolder<Any, ViewBinding>, 0, payload)
         verify(trackHolder).applyPayload(captor.capture())
         assert(payload == captor.firstValue)
+    }
+
+    @Test
+    fun `onBindViewHolder - call with empty payload - verify holder applyPayload not called`() {
+        val adapter = AnyTypeAdapter().apply {
+            diffStrategy = DiffStrategy.DiscardLatest(dispatcher)
+        }
+        AnyTypeCollection.Builder()
+            .add(listOf(Track(), Track()), trackDelegate)
+            .build()
+            .let { adapter.setCollection(it) }
+        val payload = mutableListOf<Any>()
+        adapter.onBindViewHolder(trackHolder as AnyTypeViewHolder<Any, ViewBinding>, 0, payload)
+        verify(trackHolder, times(0)).applyPayload(anyList())
+    }
+
+    @Test
+    fun `onBindViewHolder - call with overload with no payload - verify holder applyPayload not called`() {
+        val adapter = AnyTypeAdapter().apply {
+            diffStrategy = DiffStrategy.DiscardLatest(dispatcher)
+        }
+        AnyTypeCollection.Builder()
+            .add(listOf(Track(), Track()), trackDelegate)
+            .build()
+            .let { adapter.setCollection(it) }
+        adapter.onBindViewHolder(trackHolder as AnyTypeViewHolder<Any, ViewBinding>, 0)
+        verify(trackHolder, times(0)).applyPayload(anyList())
     }
 }
